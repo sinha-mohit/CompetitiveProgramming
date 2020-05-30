21065186c815e7302cecfbcee0f92d4328ba43fb
CoderMohitSinha <mohitsinha1994@gmail.com>
2020-05-30 13:16:22 +0530

    update

--- Strings/KMP.cpp
+++ Strings/KMP.cpp
@@ -1,61 +1,29 @@
 // C++ program for implementation of KMP pattern searching 
 // algorithm 
+// youtube: https://www.youtube.com/watch?v=4jY57Ehc14Y&t=2s
 //#include <bits/stdc++.h> 
 #include <iostream>
 #include <cstring>
 using namespace std;
 
-void computeLPSArray(char* pat, int M, int* lps); 
-
-// Prints occurrences of txt[] in pat[] 
-void KMPSearch(char* pat, char* txt) 
-{ 
-	int M = strlen(pat); 
-	int N = strlen(txt); 
-
-	// create lps[] that will hold the longest prefix suffix 
-	// values for pattern 
-	//int lps[M]; 
-    int *lps = new int[M];
-	// Preprocess the pattern (calculate lps[] array) 
-	computeLPSArray(pat, M, lps); 
-
-	int i = 0; // index for txt[] 
-	int j = 0; // index for pat[] 
-	while (i < N) { 
-		if (pat[j] == txt[i]) { 
-			j++; 
-			i++; 
-		} 
-
-		if (j == M) { 
-			printf("Found pattern at index %d \n", i - j); 
-			j = lps[j - 1]; 
-		} 
-
-		// mismatch after j matches 
-		else if (i < N && pat[j] != txt[i]) { 
-			// Do not match lps[0..lps[j-1]] characters, 
-			// they will match anyway 
-			if (j != 0) 
-				j = lps[j - 1]; 
-			else
-				i = i + 1; 
-		} 
-	} 
-    delete[] lps;
-} 
-
 // Fills lps[] for given patttern pat[0..M-1] 
 void computeLPSArray(char* pat, int M, int* lps) 
 { 
-	// length of the previous longest prefix suffix 
+	// length of the previous longest matching proper prefix which is also a suffix 
 	int len = 0; 
 
 	lps[0] = 0; // lps[0] is always 0 
+	int i = 1; 
+
+    /*   0 1 2 3 4 5 6
+    pat: a a b a a a c
+           i
+         len
+    len = 0; i = 1;
+    lps[0] = 0;  
+    */
 
 	// the loop calculates lps[i] for i = 1 to M-1 
-	int i = 1; 
 	while (i < M) { 
 		if (pat[i] == pat[len]) { 
 			len++; 
@@ -81,6 +49,58 @@ void computeLPSArray(char* pat, int M, int* lps)
 		} 
 	} 
 } 
+
+// Prints occurrences of txt[] in pat[] 
+void KMPSearch(char* pat, char* txt) 
+{ 
+	int M = strlen(pat); 
+	int N = strlen(txt); 
+
+	// create lps[] that will hold the longest prefix suffix 
+	// values for pattern 
+	//int lps[M]; 
+    int *lps = new int[M];
+	// Preprocess the pattern (calculate lps[] array) 
+	computeLPSArray(pat, M, lps); 
+
+	int i = 0; // index for txt[] 
+	int j = 0; // index for pat[] 
+	while (i < N) { 
+		if (pat[j] == txt[i]) { 
+			j++; 
+			i++; 
+		} 
+
+		if (j == M) { 
+			printf("Found pattern at index %d \n", i - j); 
+			j = lps[j - 1]; // again reset j = lps[j - 1] to find mutiple occurences in overlapping situations
+		} 
+
+		// mismatch after j matches 
+		else if (i < N && pat[j] != txt[i]) { 
+			// Do not match lps[0..lps[j-1]] characters, 
+			// they will match anyway 
+			if (j != 0) 
+				j = lps[j - 1]; /*j - 1 because present jth char match ni hua hai 
+                and i which is index for txt[] is going to be same*/
+			else{// if(j == 0)
+				i = i + 1; 
+                /*j is the index for pattern and here j is equal to zero.
+                and if (pat[j] != txt[i]) that is pat[0] != txt[0] that is
+                first character is not same between text and pattern, then just
+                increment or go to next char in text i.e increment i 
+                          0 1 2 3 4 5
+                eg: Text: a b c a b c
+                              i 
+                    patt: x y z
+                          j
+                */
+            }
+		} 
+	} 
+    delete[] lps;
+} 
+
 
 // Driver program to test above function 
 int main() 
